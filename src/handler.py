0bfe35025e97c6bd57fb7341fbb0f5f0e5e7c795
r2cool <r2cool@gmail.com>
2024-10-23 15:09:09 +0000

    Added base64 conver function

--- src/handler.py
+++ src/handler.py
@@ -1,18 +1,45 @@
-""" Example handler file. """
-
+from runpod.serverless.utils import download_files_from_urls, rp_cleanup, rp_debugger
+from runpod.serverless.utils.rp_validator import validate
 import runpod
 
 # If your handler runs inference on a model, load the model here.
 # You will want models to be loaded into memory before starting serverless. 
 
+def base64_to_tempfile(base64_file: str) -> str:
+    '''
+    Convert base64 file to tempfile.
 
+    Parameters:
+    base64_file (str): Base64 file
+
+    Returns:
+    str: Path to tempfile
+    '''
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_file:
+        temp_file.write(base64.b64decode(base64_file))
+
+    return temp_file.name
+
+@rp_debugger.FunctionTimer
 def handler(job):
     """ Handler function that will be used to process jobs. """
     job_input = job['input']
 
-    name = job_input.get('name', 'World')
+    if not job_input.get('audio', False) and not job_input.get('audio_base64', False):
+        return {'error': 'Must provide either audio or audio_base64'}
 
-    return f"Hello, {name}!"
+    if job_input.get('audio', False) and job_input.get('audio_base64', False):
+        return {'error': 'Must provide either audio or audio_base64, not both'}
+
+    if job_input.get('audio', False):
+        with rp_debugger.LineTimer('download_step'):
+            audio_input = download_files_from_urls(job['id'], [job_input['audio']])[0]
+
+    if job_input.get('audio_base64', False):
+        audio_input = base64_to_tempfile(job_input['audio_base64'])
+    
+
+    return f"Hello!"
 
 
 runpod.serverless.start({"handler": handler})
